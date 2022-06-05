d6e7e8a8b46bd01dbd7df52756472049ed5525e5
Panagiotis Xanthopoulos <62390731+panos511@users.noreply.github.com>
2022-06-05 20:05:44 +0300

    Update db_connection.php
    evala rootroot kwdiko gt tha parousiasoume apo linux kai exw valei rootroot sto laptop mou

--- elidek/db_connection.php
+++ elidek/db_connection.php
@@ -3,7 +3,7 @@
    function OpenCon(){
       $dbhost = "localhost";
       $dbuser = "root";
-      $dbpass = "";
+      $dbpass = "rootroot";
       $db = "elidek";
       
       $conn = new mysqli($dbhost, $dbuser,$dbpass, $db) or die("Connect failed: %s\n". $conn -> error);
